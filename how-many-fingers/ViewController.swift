f072e7fe19a9e4631fd84222158bf05887255602
Jen Homann <jenhomann@Jens-MBP.igs2.com>
2017-11-15 16:33:28 -0600

    basic how many fingers app complete

--- how-many-fingers/ViewController.swift
+++ how-many-fingers/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var guessTF: UITextField!
+    @IBOutlet var responseLabel: UILabel!
+    
+    @IBAction func guessButton(_ sender: UIButton) {
+        let guess = guessTF.text
+        let answer = arc4random_uniform(6)
+        
+        if guess != nil && guess?.rangeOfCharacter(from: CharacterSet.decimalDigits.inverted) == nil {
+            if Int(guess!) == Int(answer) {
+                responseLabel.text = "You're right!"
+            } else {
+                responseLabel.text = "Nope. It's not \(guess!). Try again."
+            }
+        } else {
+            responseLabel.text = "You must enter a number to play."
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
